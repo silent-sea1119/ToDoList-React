47c80adb61004b51bc6858f400755e4ee198790e
silent-sea1119 <sandragonzales56t@gmail.com>
2016-03-15 01:18:51 +0100

    add main components

--- todo.js
+++ todo.js
@@ -1,9 +1,43 @@
+
+var SearchBox = React.createClass({
+	render:function(){
+		return (
+				<header className='header'>
+					<h1>To Do List</h1>
+					<input className="new-todo" type="text" placeholder="what will I do"></input>
+					<input className="toggle-all" type="checkbox"></input>
+				</header>
+			)
+	}
+})
+
+var OutcomeList = React.createClass({
+	render:function(){
+		return (
+				<section className='main'>
+					<ul className="todo-list">
+					</ul>
+				</section>
+			)
+	}
+})
+
+var Footer = React.createClass({
+	render:function(){
+		return (
+				<footer className="footer">
+				</footer>
+			)
+	}
+})
+
 var CommonContainer = React.createClass({
 	render:function(){
 		return (
 			<div className="todoapp">
-				<h1>To Do List</h1>
-				<input className="new-todo" type="text"></input>
+				<SearchBox />
+				<OutcomeList />
+				<Footer />
 			</div>
 		)
 	}
