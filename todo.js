35635c290442a41058462767154ab1b36d4400e3
silent-sea1119 <sandragonzales56t@gmail.com>
2016-03-15 20:27:27 +0100

    add filter and improve the delete function

--- todo.js
+++ todo.js
@@ -17,7 +17,7 @@ var SearchBox = React.createClass({
 		return {text:""};
 	},
 	test:function(e){
-		if(e.keyCode===13){
+		if(e.keyCode===13 && this.state.text!==""){
 			this.props.addTodoitem(new Todowork(this.state.text));
 			this.setState({text:""});
 		}
@@ -38,8 +38,8 @@ var SearchBox = React.createClass({
 })
 
 var OutcomeList = React.createClass({
-	deleteTodoitem:function(index,e){
-		this.props.deleteTodoitem(index);
+	deleteTodoitem:function(element,e){
+		this.props.deleteTodoitem(element);
 	},
 
 	render:function(){
@@ -52,7 +52,7 @@ var OutcomeList = React.createClass({
 					<div className="view">
 						<input className="toggle" type="checkbox" checked={element.isCompleted} onClick={self.props.toggleItem.bind(self,index)}></input>
 						<label>{element.name}</label>
-						<button  onClick={self.deleteTodoitem.bind(self,index)} className="destroy"></button>
+						<button  onClick={self.deleteTodoitem.bind(self,element)} className="destroy"></button>
 					</div>
 				</li>
 			);
@@ -68,10 +68,25 @@ var OutcomeList = React.createClass({
 })
 
 var Footer = React.createClass({
+	updateFilter:function(e){
+		this.props.updateFilter(e.target.text);
+	},
 	render:function(){
+		var self=this;
+		var keyArray = ["All","Active","Completed"];
+		var selectButton = this.props.filterWord;
+		var nodes = keyArray.map(function(element){
+			var selected = element===selectButton?"selected":"";
+			return (<li>
+						<a className={selected} onClick={self.updateFilter}>{element}</a>
+				   </li>)
+		});
 		return (
 				<footer className="footer">
 					<span className="todo-count">To be Done:{this.props.todocount}</span>
+					<ul className='filters'>
+							{nodes}
+					</ul>
 				</footer>
 			)
 	}
@@ -79,26 +94,57 @@ var Footer = React.createClass({
 
 var CommonContainer = React.createClass({
 	getInitialState:function(){
-		return {data:[],todocount:0};
+		return {data:[],filterdata:[],todocount:0,filterWord:"All"};
+	},
+	updateFilter:function(text){
+		var oldKey = this.state.filterWord;
+		var filterdata = this.state.filterdata;
+		if(oldKey!==text){
+			filterdata=this.updateData(this.state.data,text);
+		}
+		this.setState({filterWord:text,filterdata:filterdata});
+		
+	},
+	updateData:function(data,text=this.state.filterWord){
+		var data = data;
+		if(text==="Active"){
+			data=data.filter(function(item){
+				return !item.isCompleted;
+			});
+			console.log("Active",data);
+		}else if(text==="Completed"){
+			data=data.filter(function(item){
+				return item.isCompleted;
+			})
+			console.log("Completed",data);
+		}
+		return data;
 	},
 	addTodoitem:function(obj){
 		var item = obj;
 		var data = this.state.data;
 		data.unshift(item);
 		var count = this.state.todocount+1;
-		this.setState({data:data,todocount:count});
+		var filterdata=this.updateData(data);
+		this.setState({data:data,todocount:count,filterdata:filterdata});
 	},
-	deleteTodoitem:function(number){
+	deleteTodoitem:function(element){
 		var data = this.state.data;
-		delete data[number];
+		delete data[data.indexOf(element)];
 		var count=this.updateTodo(data);
-		this.setState({data:data,todocount:count});
+		console.log("deleted before",data);
+		var filterdata=this.updateData(data);
+		console.log("deleted",filterdata);
+		this.setState({data:data,todocount:count,filterdata:filterdata});
 	},
 	toggleItem:function(index){
 		var data = this.state.data;
 		data[index].toggle();
 		var count=this.updateTodo(data);
-		this.setState({data:data,todocount:count});
+		var filterdata=this.updateData(data);
+		this.setState({data:data,todocount:count,filterdata:filterdata});
+
+
 	},
 	toggleAll:function(){
 		var data = this.state.data;
@@ -106,7 +152,9 @@ var CommonContainer = React.createClass({
 			item.toggle();
 		});
 		var count=this.updateTodo(data);
-		this.setState({data:data,todocount:count});
+		var filterdata=this.updateData(data);
+		this.setState({data:data,todocount:count,filterdata:filterdata});
+
 	},
 	updateTodo:function(data){
 		var count = 0;
@@ -121,8 +169,8 @@ var CommonContainer = React.createClass({
 		return (
 			<div className="todoapp">
 				<SearchBox addTodoitem={this.addTodoitem} toggleAll={this.toggleAll} addTodoitem={this.addTodoitem}/>
-				<OutcomeList items={this.state.data} toggleItem={this.toggleItem} deleteTodoitem={this.deleteTodoitem}/>
-				<Footer todocount={this.state.todocount}/>
+				<OutcomeList items={this.state.filterdata} toggleItem={this.toggleItem} deleteTodoitem={this.deleteTodoitem}/>
+				<Footer todocount={this.state.todocount} filterWord={this.state.filterWord} updateFilter={this.updateFilter}/>
 			</div>
 		)
 	}
